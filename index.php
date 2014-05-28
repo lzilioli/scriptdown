fc309f32f962cc6e3eed5c7bde7f4e123637cd25
Luke Zilioli <elzee08@gmail.com>
2014-05-28 10:48:31 -0400

    Reorder index.php for clarity

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<? $cmd = 'python scriptdown.py samples/mofamdd.sd' ?>
-
 <!DOCTYPE html>
 <html>
 	<head>
 		<link href="www/gui/build/css/tv.css" rel="stylesheet" type="text/css">
 	</head>
-	<?= `$cmd` ?>
+	<body>
+		<? $cmd = 'python scriptdown.py samples/mofamdd.sd' ?>
+		<?= `$cmd` ?>
+	</body>
 </html>
